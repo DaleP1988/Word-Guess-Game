c53b4bdfc6ce8a4f558bdc20ab5ab208d66eb394
Garnet <garnet.dianthus@gmail.com>
2018-06-29 20:38:51 -0700

    styling adds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,178 @@
 
+        var remainingWrong = 5;
+        var wins = 0;
+        var lastResult = null;
+        var word;
+        var correctWord;
+        var answerArray;
+        var update;
+        var lettersGuessed;
+        var words = [
 
-var words = [
+            "Basie",
+            "Armstrong",
+            "Reinhardt",
+            "Ellington",
+            "Gershwin",
+            "Beiderbecke",
+            "Smith",
+            "Baker"
 
-    "Bassie", 
-    "Armstrong", 
-    "Reinhardt", 
-    "Ellington", 
-    "Gershwin", 
-    "Beiderbecke", 
-    "Smith", 
-    "Baker"
-
-];
-
-//click any key to get started
-
-document.onkeyup = function ()
-{ var userguess = String.fromCharCode(event.keyCode)|
-    toLowerCase();
-
-    console.log(userguess);
-}
-
-// start of game code
-
-var word = words [Math.floor(Math.random() * words.length + 4)];
-
-var answerArray = [];
-for (var i = 0;i < word.length; i++){
-    answerArray[i]="_";
-
-console.log(answerArray);
-}
-
-
-//show player progress: update answer array and display
-
-function remaining() {
-var remainingletters = word.length;
-while (remainingletters >  0);
-
-    
-}
-
-
-//wins
-
-//guesses remaining
-
-//letters already guessed
-
-if(guess === null) {
-    
+        ];
+        $(document).ready(function () {
 
 
 
-} else {
 
-}
+            //click any key to get started
 
-for (var j = 0; j < word.length; j++){
-    if (word[j] === guess){
-    answerArray[j] = guess;
-    remainingletters-- ;
+            document.onkeyup = function () {
+                var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-    }
-}
+                checkGuess(userGuess);
 
-// display answer. THIS IS WRONG.
+            }
 
-{
-alert(answerArray.join (""));
-alert("You Got it! The answer is" + word);
+            function checkGuess(guess) {
+                var found = false;
+                for (var j = 0; j < correctWord.length; j++) {
+                    if (correctWord[j].toLowerCase() === guess) {
+                        answerArray[j] = guess;
+                        correctWord = replaceAt(correctWord, j, " ");
+                        found = true;
+                        break;
 
-}
+                    }
 
-//need to make sure I am referencing the correct variables...
 
-var html = "<p>CLICK ANY KEY TO GET STARTED!</p>" +
-"<p>wins: " +  wins   + "</p>"  +
-"<p>current word: " +  current   + "</p>"  +
-"<p>number of guesses remaining: " +  remaining  +   "</p>" +
-"<p>letters already guessed: " + guessed +  "</p>";
+                }
 
-document.querySelector('#game').innerHTML = html;
+                if (!found) {
+                    lettersGuessed.push(guess);
+                    remainingWrong--;
 
-//VARIABLES I STILL NEED:current word, guesses remaining, letters already guessed
+                    if (remainingWrong == 0) {
+
+                        lastResult = false;
+
+                    }
+
+
+                } else {
+                    if (correctWord.trim() == "") {
+
+                        wins++;
+                        lastResult = true;
+
+                    }
+
+                }
+
+                update();
+
+            }
+
+
+            var replaceAt = function (text, index, replacement) {
+                return text.substr(0, index) + replacement + text.substr(index + replacement.length);
+            }
+
+
+            // start of game code
+
+
+
+
+
+            update = function () {
+                var html = "<p>";
+                for (var i = 0; i < answerArray.length; i++) {
+                    html += answerArray[i] + "  ";
+
+                    //console.log(answerArray);
+                }
+
+                html += "</p>";
+                if (lastResult != null) {
+                    var result = lastResult ? "win" : "lose";
+
+                    html += "<p> you " + result + "!</p>";
+                    
+                   
+                    $("#newGame").show();
+
+                }else{
+
+
+                    html += "<p> remaining guesses:" + remainingWrong + "</p>";
+                    html += "<p> letters guessed:" + lettersGuessed.toString() +  "</p>";
+
+                }
+
+
+                html += "<p> you have " + wins + " wins  </p>";
+
+
+                $('#game').html(html);
+
+
+            };
+
+            newGameBtnClick( );
+
+
+
+
+            /*if(guess === null) { 
+            
+            
+            
+            
+            } */
+
+
+
+
+            //alert(answerArray.join (""));
+            function showSuccess() {
+
+                alert("You Got it! The answer is" + word);
+
+            }
+
+            //need to make sure I am referencing the correct variables...
+
+            /*var html = "<p>CLICK ANY KEY TO GET STARTED!</p>" +
+            "<p>wins: " +  wins   + "</p>"  +
+            "<p>current word: " +  current   + "</p>"  +
+            "<p>number of guesses remaining: " +  remaining  +   "</p>" +
+            "<p>letters already guessed: " + guessed +  "</p>";
+            
+            document.querySelector('#game').innerHTML = html; */
+
+            //VARIABLES I STILL NEED:current word, guesses remaining, letters already guessed
+
+
+        });
+
+        function newGameBtnClick() {
+            remainingWrong = 5;
+            word = words[Math.floor(Math.random() * words.length)];
+            correctWord = word;
+            lettersGuessed = [];
+            answerArray = [];
+            for (var i = 0; i < word.length; i++) {
+                answerArray[i] = "_";
+
+
+
+
+            }
+            lastResult = null;
+
+            update();
+
+            $("#newGame").hide();
+
+           
+        }
